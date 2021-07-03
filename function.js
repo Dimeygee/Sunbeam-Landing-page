b70a2c832a112d111f934e73b680e0b382367060
Dimeygee <untamed737@gmail.com>
2021-07-03 01:23:12 +0100

    initial

--- function.js
+++ function.js
@@ -1,3 +1,71 @@
+
+       
+    let links = document.querySelectorAll('ul li a')
+    let resp_links = document.querySelectorAll('.resp_menu ul li a')
+    let sections = document.querySelectorAll('section[id]')
+    let header = document.querySelector('header')
+    let nav = document.querySelector('nav')
+
+ 
+    
+
+    window.addEventListener('load', pageLoad)
+    function pageLoad(){
+        links[0].classList.add('active')
+        resp_links[0].classList.add('active')
+    }
+
+    console.log(window.pageYOffset,header.offsetHeight)
+
+    function checkWithin(){
+        if(window.pageYOffset >= header.offsetTop && window.pageYOffset <= header.offsetHeight + header.offsetTop - 150){
+            return true
+        }
+        return false
+    }
+
+  
+
+    window.addEventListener('scroll', addActive)
+
+    function addActive(){
+        if(window.pageYOffset > header.offsetHeight - 100){
+            document.querySelector('nav').classList.add('active')
+        }  
+        else{
+            document.querySelector('nav').classList.remove('active')
+        }
+       if(!checkWithin()){
+            links[0].classList.remove('active')
+            resp_links[0].classList.remove('active')
+            window.removeEventListener('load', pageLoad)
+       }else{
+            links[0].classList.add('active')
+            resp_links[0].classList.add('active')
+       }
+
+       let scrollY = window.pageYOffset
+
+       sections.forEach(section => {
+            let sectionHeight = section.offsetHeight
+            let sectionTop = section.offsetTop - 150
+            let sectionId = section.getAttribute('id')
+
+            if(scrollY  > sectionTop && scrollY <= sectionTop + sectionHeight){
+                
+                 
+                  document.querySelector(`ul li a[href*="${sectionId}"]`).classList.add('active')
+                  document.querySelector(`.resp_menu div ul li a[href*="${sectionId}"]`).classList.add('active')
+              }else{
+                  document.querySelector(`ul li a[href*="${sectionId}"]`).classList.remove('active')
+                  document.querySelector(`.resp_menu div ul li a[href*="${sectionId}"]`).classList.remove('active')
+              }
+
+        })
+
+    }
+
+  
    function toggleMenu() {
                     const menus = document.querySelectorAll('.hamburger');
                     const sm_menu = document.querySelector('.resp_menu')
@@ -7,74 +75,4 @@
                     sm_menu.classList.toggle('active');
                 }
 
-        
-        let links = document.querySelectorAll('ul li a')
-        let resp_links = document.querySelectorAll('.resp_menu ul li a')
-        let sections = document.querySelectorAll('section[id]')
-
-        let hH = document.querySelector('header').offsetHeight
-
-        links[0].classList.add('active')
-        resp_links[0].classList.add('active')
-
-        links.forEach(link => {
-            link.addEventListener('click', addActive)
-        })
-        function addActive(e){
-            links.forEach(link => {
-                link.classList.remove('active')
-            })
-            e.target.classList.add('active')
-
-        }      
-
-         window.addEventListener('scroll', addActiveOnScroll)
-
-        function addActiveOnScroll(){
-         if(window.pageYOffset > hH - 100){
-                document.querySelector('nav').classList.add('active')
-            }  
-            else{
-                document.querySelector('nav').classList.remove('active')
-            }
-           
-          let scrollY = window.pageYOffset
-
-            sections.forEach(section => {
-                let sectionHeight = section.offsetHeight
-                let sectionTop = section.offsetTop - 150
-                let sectionId = section.getAttribute('id')
-
-                if(scrollY  > sectionTop && scrollY <= sectionTop + sectionHeight){
-                  /*  links.forEach(link => {
-                        
-                        if(scrollY > hH){
-                            link.classList.remove('active')
-                        }else{
-                            link.classList.add('active')
-                        }
-                    })*/
-
-                    if(scrollY > hH){
-                        links[0].classList.remove('active')
-                        resp_links[0].classList.remove('active')
-                    }else{
-                        links[0].classList.add('active')
-                        resp_links[0].classList.add('active')
-                    }
-
-                    resp_links.forEach(link => {
-                        link.classList.remove('active')
-                    })
-                    
-                   
-                    document.querySelector(`ul li a[href*="${sectionId}"]`).classList.add('active')
-                    document.querySelector(`.resp_menu div ul li a[href*="${sectionId}"]`).classList.add('active')
-                }else{
-                    document.querySelector(`ul li a[href*="${sectionId}"]`).classList.remove('active')
-                    document.querySelector(`.resp_menu div ul li a[href*="${sectionId}"]`).classList.remove('active')
-                }
-            })
-
-            
-        }
+     
